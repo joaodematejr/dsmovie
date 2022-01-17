acdbefd17025b47391a717b095b531129ab44a1a
joaodematejr <joaodematejr@gmail.com>
2022-01-17 19:17:21 -0300

    Atualização geral

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,26 +1,23 @@
-import { ReactComponent as GithubIcon } from "assets/Img/github.svg";
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
 import './styles.css';
 
-function NavBar() {
-  return (
-    <header>
-      <nav className="container">
-        <div className="dsmovie-nav-content">
-          <h1>DSMovie</h1>
-          <a
-            href="https://github.com/joaodematejr"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <div className="dsmovie-contact-container">
-              <GithubIcon />
-              <p className="dsmovie-contact-link">/devsuperior</p>
-            </div>
-          </a>
-        </div>
-      </nav>
-    </header>
-  );
+function Navbar() {
+
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/devsuperior">
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p className="dsmovie-contact-link">/devsuperior</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
 }
 
-export default NavBar;
+export default Navbar;
